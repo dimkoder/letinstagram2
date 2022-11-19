75606f5a399a4d4df923486cc6873c56782a2663
Dima Laz <Udimuz@gmail.com>
2022-11-19 08:18:35 +0300

    Initial commit 3 .env

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
+/* DDDDDDD
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
